df2dc3fb7606977a0068750b6a3b2cfd8cd24953
Jose Pablo Nordenflycht <diabhlo@gmail.com>
2020-08-19 21:19:05 -0400

    ARGV listo

--- desafio1.rb
+++ desafio1.rb
@@ -1,9 +1,6 @@
-puts 'Introduzca el primer numero '
-a = gets.chomp.to_i
-puts 'Introduzca el segundo numero '
-b = gets.chomp.to_i
-puts 'Introduzca el tercer numero '
-c = gets.chomp.to_i
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+c = ARGV[2].to_i
 
 puts "el numero mayor es #{a}" if a > b && a > c
 puts "el numero mayor es #{b}" if b > c && b > a
